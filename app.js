8dc0fef1268879781b959c657c255b08fb7b1be9
valstrand <84023324+valstrand@users.noreply.github.com>
2024-08-05 19:34:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "./firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
