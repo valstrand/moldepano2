21ccceccd219abf2b85ce444de00510f77806ab4
valstrand <84023324+valstrand@users.noreply.github.com>
2024-08-05 19:58:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,14 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 let nameCell = row.insertCell(0);
                 let areaCell = row.insertCell(1);
                 let heightCell = row.insertCell(2);
-                let climbedCell = row.insertCell(3);
-                let commentsCell = row.insertCell(4);
 
                 nameCell.textContent = mountain.name;
                 areaCell.textContent = mountain.area;
                 heightCell.textContent = mountain.height;
-                climbedCell.textContent = mountain.climbed;
-                commentsCell.textContent = mountain.comments;
             });
         })
         .catch(error => console.error('Error fetching the mountains data:', error));
